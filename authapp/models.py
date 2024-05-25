f4b2ff269120c3b4b297a2318ac7bf65b76b4a0f
Taborda <adryanftaborda@gmail.com>
2024-05-24 23:42:17 -0300

    main server configs, models and views started

--- authapp/models.py
+++ authapp/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 # Create your models here.
 
 class User(AbstractUser):
-    gender_options = ['MALE','FEMALE']
-    name = models.CharField(max_length=20)
+    class genders(models.TextChoices):
+        MALE = 'MALE'
+        FEMALE = 'FEMALE'
+    
+    username = models.CharField(max_length=20,unique=True)
     email = models.EmailField()
     password = models.CharField(max_length=50)
-    gender = models.CharField(choices=gender_options)
+    gender = models.CharField(max_length=6,choices=genders.choices,default=None)
     birthday = models.DateField()
 
     def verify_gender():
-        if User.gender not in User.gender_options:
-            return 'Please, consider the specified genders.'
+        super().clean()
+        if User.gender not in User.genders:
+            raise ValidationError("Choice must be either 1 or 2.")
+        return User.gender
+        
+    USERNAME_FIELD = 'username'
