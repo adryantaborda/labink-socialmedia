7ef4ffbb1e7064b07968381be95aabe556613bd0
Taborda <adryanftaborda@gmail.com>
2024-05-25 15:00:35 -0300

    login view updated and temporary forms
    Former-commit-id: 6824b8e05af612afb635b55f847cc36d27a7c09f

--- authapp/forms.py
+++ authapp/forms.py
@@ -5,4 +5,4 @@ from django.contrib.auth.forms import UserCreationForm
 class   MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username','email','password1','password2','gender','birthday']
+        fields = ['username','email','password1','password2',]
