22d0855595a333f989db4643f99319673504cbba
Taborda <adryanftaborda@gmail.com>
2024-05-24 23:42:17 -0300

    main server configs, models and views started

---
+++ authapp/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class   MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2','gender','birthday']
