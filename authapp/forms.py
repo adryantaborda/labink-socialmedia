1836aff984228f8b90f0662b16bac38db57e1482
Taborda <adryanftaborda@gmail.com>
2024-06-04 17:12:15 -0300

    Functionality of checking length, characters, etc of a password

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,9 +1,11 @@
 from django.forms import ModelForm
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms   
 
 class   MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2','gender','birthday']
+
         
