00293daad5e27c5ba81059a4fbeac68c56ada409
Taborda <adryanftaborda@gmail.com>
2024-06-05 17:20:24 -0300

    user profile
    Former-commit-id: 89edf586d6becc426dac66ac9168235fea8b3af2

--- authapp/functions.py
+++ authapp/functions.py
@@ -3,13 +3,16 @@ import re
 def check_strength(password):
     if len(password) < 8:
         return False, "Password must be at least 8 characters long."
-    if not re.search(r"[A-Z]", password):
-        return False, "Password must contain at least one uppercase letter."
-    if not re.search(r"[a-z]", password):
-        return False, "Password must contain at least one lowercase letter."
+    if not re.search(r"[a-zA-Z]", password):
+        return False, "Password must contain letters"
     if not re.search(r"\d", password):
         return False, "Password must contain at least one number."
     if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
         return False, "Password must contain at least one special character."
     
     return True, "Password is strong."
+
+def clean_username(username):
+    cleanspaces = username.replace(" ","")
+    cleanusername = cleanspaces.lower()
+    return cleanusername
