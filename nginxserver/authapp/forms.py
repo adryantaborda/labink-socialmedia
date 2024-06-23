71511961d7335749ebb714a77397e450903905d8
Taborda <adryanftaborda@gmail.com>
2024-06-23 17:09:36 -0300

    many additions related to server

---
+++ nginxserver/authapp/forms.py
@@ -0,0 +1,18 @@
+from django.forms import ModelForm
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms   
+from datetime import date
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2','gender','birthday']
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = User 
+        fields = ['bio','profile_picture']
