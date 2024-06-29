83848a5af5ce36ff2e7147228a63a1cd9fc1d027
Taborda <adryanftaborda@gmail.com>
2024-06-28 22:18:52 -0300

    ...

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from .models import User, ConnectionRequest, UserConnections
 from django.contrib.auth.admin import UserAdmin
-from image_cropping import ImageCroppingMixin
 
-class MyModelAdmin(ImageCroppingMixin, admin.ModelAdmin):
-    pass
-
-admin.site.register(User,MyModelAdmin)
+admin.site.register(User,admin.ModelAdmin)
 admin.site.register(ConnectionRequest)
 admin.site.register(UserConnections)
 
--- authapp/models.py
+++ authapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from image_cropping import ImageRatioField, ImageCropField
 from PIL import Image
 from django.conf import settings
 from datetime import date
